b1c936a5f998da426a2dc34af926bbcb7568cba9
Vetrivel9004 <vetrivel7697@gmail.com>
2024-09-17 08:43:13 +0530

    vetri Ecommerce project

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
@@ -115,6 +116,5 @@ app.get("/product",async(req,res)=>{
 
 })
  
-
   //Server running
  app.listen(PORT, () => console.log("Server is running at port: " + PORT));
